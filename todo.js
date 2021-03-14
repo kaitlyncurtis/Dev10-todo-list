f4ba76109362a1a2ecfb8e04147d6d3b32dff51a
kaitlyncurtis <kaitlyn.curtis@gmail.com>
2021-03-13 18:48:02 -0600

    actually creating alerts now
    i added code to the createAlert function that gets the task and timeLeft from the user, places that in the appropriate variables, and creates a new <p> to be appended to the HTML doc.
    
    also added a <p> container to the HTML doc that I am appending all the additions to but maybe that should be a <div>?

--- todo.js
+++ todo.js
@@ -1,4 +1,15 @@
 function createAlert() {
     let task = document.getElementById("task").value;
     let timeLeft = document.getElementById("days").value;
+
+    let alerts = document.getElementById("alerts");
+    let newAlert = document.createElement("p");
+    newAlert.innerHTML = `${task}<br>${timeLeft} day(s)`
+
+    alerts.appendChild(newAlert);
 }
+
+//.appendChild() is a method that can be used on DOM ... nodes ? 
+//think this is how I can add new little objects for each task
+//reminder to self to set the inputs to clear after the submit button is pressed
+//reminder to self to set focus on the task input box at some point
