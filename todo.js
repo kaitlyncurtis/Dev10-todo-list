4b03cf2ed1d7c9ea30a20f4cbfa452837eb71f6c
kaitlyncurtis <kaitlyn.curtis@gmail.com>
2021-03-16 22:46:03 -0500

    work on HTML5 validation and JS and Bootstrap
    1. tried a few things to get HTML5 validation to work
    2. worked on the JS code for getting task info and creating alerts
    3. found out how to create "alerts" (a Bootstrap component)! Each new alert breaks to a new line which I will have to figure out.

--- todo.js
+++ todo.js
@@ -1,9 +1,12 @@
-function createAlert() {
-    let task = document.getElementById("task").value;
-    let timeLeft = document.getElementById("days").value;
+let task = ""
+let timeLeft = ""
 
+function createAlert() {
     let alerts = document.getElementById("alerts");
-    let newAlert = document.createElement("p");
+    let newAlert = document.createElement("div");
+
+    newAlert.className = "alert alert-primary";
+    newAlert.setAttribute("role", "primary");
     newAlert.innerHTML = `${task}<br>${timeLeft} day(s)`
 
     alerts.appendChild(newAlert);
@@ -12,6 +15,15 @@ function createAlert() {
     document.getElementById("days").value = "";
     document.getElementById("task").focus()
 }
+
+function getTask() {
+    task = document.getElementById("task").value;
+    timeLeft = document.getElementById("days").value;
+
+    createAlert()
+}
+
+
 
 //.appendChild() is a method that can be used on DOM ... nodes ? 
 //think this is how I can add new little objects for each task
