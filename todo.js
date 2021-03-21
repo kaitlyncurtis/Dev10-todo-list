d0b65759c4e8730eebdec53074c83fb9e2d7a546
kaitlyncurtis <kaitlyn.curtis@gmail.com>
2021-03-21 11:22:44 -0500

    still trying
    still not quite there yet with event listeners, will keep working on it

--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
 let task = document.getElementById("task");
 let timeLeft = document.getElementById("days");
-/*
+
 task.addEventListener("input", function(event) {
     if(task.validity.valueMissing) {
         task.setCustomValidity("Please enter a value");
@@ -41,9 +41,10 @@ function createAlert() {
     //can I change positioning of button within the alert?
     alerts.appendChild(newAlert);
 
-    document.getElementById("task").value = "";
-    document.getElementById("days").value = "";
-    document.getElementById("task").focus()
+    //document.getElementById("task").value = "";
+    //document.getElementById("days").value = "";
+    document.getElementById("taskform").reset();
+    //document.getElementById("task").focus();
 }
 
 function getTask() {
@@ -76,19 +77,9 @@ function getTask() {
 //they use event listeners and you can set custom validity messages based on certain conditions
 //i think that's the way to go. gonna start a new branch off main n try it.
 
-//the above function not working :/ when I enter both fields correctly it doesn't create alert, 
-//but it doesn't print error message to the console
-//if I don't fill any field and hit submit, it kicks the last console error message, saying to enter a number >=1
-//must be another way to tell whether a field is filled or not?
-
-//fixed up validation!!!! to check if a field is filled, checking the boolean expression variable.length == 0
-//this seemed to solve all my issues and it seems like the HTML5 validation is now working as well
-
-//ok I've realized that I had written my getTask function so that any error case would throw
-//an error message to the CONSOLE
-//so in an error case, it's still doing something, which causes the if-else statements to conclude,
-//so that the HTML5 validation can do it's thing? and an alert isn't created inadvertently?
-//maybe there's an equivalent of a break statement I can use that can accomplish the same
+//thoughts: i want the error message to only display when the button is clicked
+//i want the form to clear only when alert is created
+//i went throught Content Validation API example on MDN which I think can do what I want
 
 //is it possible to clear both inputs with like a querySelector or more general get selector?
 //ok the querySelectorAll didn't work, it didn't change the value of those two inputs 
